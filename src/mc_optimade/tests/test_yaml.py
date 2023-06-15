d89a7849c3392986214abd55691de600078fa6cb
Matthew Evans <git@ml-evs.science>
2023-06-15 10:16:52 +0200

    Restructure package and add pyproject

--- src/mc_optimade/tests/test_yaml.py
+++ src/mc_optimade/tests/test_yaml.py
@@ -1,8 +1,8 @@
 from pathlib import Path
-from . import Config
+from mc_optimade import Config
 import pytest
 
-EXAMPLE_YAMLS = (Path(__file__).parent / "examples").glob("*/optimade.yaml")
+EXAMPLE_YAMLS = (Path(__file__).parent.parent / "examples").glob("*/optimade.yaml")
 
 @pytest.mark.parametrize("path", EXAMPLE_YAMLS)
 def test_example_yaml(path):
