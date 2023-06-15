5b3acffd67e5a30b076814a20349b2c0342ec794
Matthew Evans <git@ml-evs.science>
2023-06-15 10:43:15 +0200

    More restructing and linting

--- src/mc_optimade/tests/test_yaml.py
+++ src/mc_optimade/tests/test_yaml.py
@@ -1,10 +1,12 @@
 from pathlib import Path
-from mc_optimade import Config
+
 import pytest
 
+from mc_optimade import Config
+
 EXAMPLE_YAMLS = (Path(__file__).parent.parent / "examples").glob("*/optimade.yaml")
+
 
 @pytest.mark.parametrize("path", EXAMPLE_YAMLS)
 def test_example_yaml(path):
     assert Config.from_file(path)
-
