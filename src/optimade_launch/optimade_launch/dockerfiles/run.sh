8705551c28462158c58556cb56d7edc6d49e6917
Jusong Yu <jusong.yeu@gmail.com>
2023-06-16 18:48:12 +0200

    test socket

---
+++ src/optimade_launch/optimade_launch/dockerfiles/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mkdir -p logs
+mkdir -p /tmp
+
+touch logs/mc_optimade_access.log
+touch logs/mc_optimade_error.log
+
+tail -f -n 20 logs/mc_optimade_access.log logs/mc_optimade_error.log &
+
+gunicorn \
+    -w 2 \
+    -k uvicorn.workers.UvicornWorker \
+    --error-logfile logs/mc_optimade_error.log \
+    --access-logfile logs/mc_optimade_access.log \
+    --capture-output \
+    --access-logformat "%(t)s: %(h)s %(l)s %(u)s %(r)s %(s)s %(b)s %(f)s %(a)s" \
+    -b unix:/tmp/gunicorn.sock optimade.server.main:app
