c4714182ab77a921149f485f05be979316f11335
superstar54 <xingwang1991@gmail.com>
2023-06-16 15:19:18 +0200

    add url as a input

--- src/mc_optimade/mc_optimade/archive/scan_records.py
+++ src/mc_optimade/mc_optimade/archive/scan_records.py
@@ -3,12 +3,11 @@ import tqdm
 from mc_optimade.archive.archive_record import ArchiveRecord
 from mc_optimade.archive.utils import get_all_records, get_parsed_records
 
-DEFAULT_ARCHIVE_API_URL = (
-    "https://archive.materialscloud.org/api/records/?sort=mostrecent&page=1&size=9999"
-)
+DEFAULT_ARCHIVE_URL = "https://archive.materialscloud.org/"
 
 
-def process_records(records):
+
+def process_records(records: list, archive_url: str=DEFAULT_ARCHIVE_URL):
     """
     Scan the Materials Cloud Archive entries, read the file info
     and check if there is a file called "optimade.y(ml|aml)".
@@ -20,17 +19,19 @@ def process_records(records):
         record_id = record["id"]
         if record_id in old_record_ids:
             continue
-        record = ArchiveRecord(record_id)
+        record = ArchiveRecord(record_id, archive_url=archive_url)
         if record.is_optimade_record():
+            print(f"Record {record_id} is a OPTIMADE record.")
             record.process()
 
 
-def scan_records(archive_api_url=DEFAULT_ARCHIVE_API_URL):
+def scan_records(archive_url=DEFAULT_ARCHIVE_URL):
     """This script can be run as a cron job to check for new optimade entries in the Materials Cloud Archive, and convert them to OPTIMADE format."""
     print("Start scanning the Materials Cloud Archive for new OPTIMADE entries...")
-    records = get_all_records(archive_api_url)
-    process_records(records)
+    records = get_all_records(archive_url)
+    process_records(records, archive_url)
 
 
 if __name__ == "__main__":
-    scan_records()
+    url = "https://staging-archive.materialscloud.org/"
+    scan_records(url)
