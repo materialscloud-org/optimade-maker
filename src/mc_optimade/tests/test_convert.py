6d362b40215e986212c9c8b2d74bc144d72ccb6f
Matthew Evans <git@ml-evs.science>
2023-10-02 12:21:23 +0200

    Add ability to compare first entry against reference data

--- src/mc_optimade/tests/test_convert.py
+++ src/mc_optimade/tests/test_convert.py
@@ -12,6 +12,13 @@ EXAMPLE_ARCHIVES = (Path(__file__).parent.parent / "examples").glob("*")
 
 @pytest.mark.parametrize("archive_path", EXAMPLE_ARCHIVES)
 def test_convert_example_archives(archive_path, tmp_path):
+    """This test will run through all examples in the examples folder and
+    attempt to convert them to OPTIMADE data following the provided config.
+
+    If a file `.testing/first_entry.json` is found, the first entry in the resulting
+    OPTIMADE API will be compared against this file.
+
+    """
     # copy example into temporary path
     tmp_path = tmp_path / archive_path.name
     shutil.copytree(archive_path, tmp_path)
@@ -19,11 +26,35 @@ def test_convert_example_archives(archive_path, tmp_path):
     jsonl_path = convert_archive(tmp_path)
     assert jsonl_path.exists()
 
+    first_entry_path = archive_path / ".testing" / "first_entry.json"
+    first_entry = None
+    if first_entry_path.exists():
+        first_entry = json.loads(first_entry_path.read_text())
+
     with open(jsonl_path, "r") as fhandle:
         # check that header exists
         header_jsonl = fhandle.readline()
         header = json.loads(header_jsonl)
         assert "x-optimade" in header
+
+        # if provided, check that the first entry matches
+        if first_entry is not None:
+            while next_entry := json.loads(fhandle.readline()):
+                if next_entry.get("type") == first_entry["type"]:
+                    break
+            else:
+                assert (
+                    False
+                ), "No structures found in archive but test first entry was provided"
+
+            # @ml-evs: species is the only key that can be written in any order, so here we
+            # just sort before comparing. This will be fixed in the next optimade-python-tools
+            if species := next_entry.get("attributes", {}).get("species"):
+                next_entry["attributes"]["species"] = sorted(
+                    species, key=lambda x: x["name"]
+                )
+
+            assert next_entry == first_entry
 
 
 def test_decompress_bz2(tmp_path):
