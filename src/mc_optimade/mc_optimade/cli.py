bcd08a780a7c2bc754975904fe818885558b06a5
Matthew Evans <git@ml-evs.science>
2023-12-06 14:08:56 +0000

    Add help string for CLI

--- src/mc_optimade/mc_optimade/cli.py
+++ src/mc_optimade/mc_optimade/cli.py
@@ -3,7 +3,10 @@ from pathlib import Path
 from mc_optimade.convert import convert_archive
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        prog="optimake",
+        description="Use an `optimade.yaml` config to describe archived data and create a OPTIMADE JSONL file for ingestion as an OPTIMADE API."
+    )
     parser.add_argument("archive_path", help="The path to the archive to ingest.")
     args = parser.parse_args()
     convert_archive(Path(args.archive_path))
