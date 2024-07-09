f6441e9eb7d3a0ef14a48e9cf306f40fedbd587b
Kristjan Eimre <kristjaneimre@gmail.com>
2024-07-09 20:08:50 +0300

    serve command; cli reorg; click

--- src/optimake/cli.py
+++ src/optimake/cli.py
@@ -3,24 +3,35 @@ from pathlib import Path
 import click
 
 from optimake.convert import convert_archive
-from optimake.serve.serve_w_docker_compose import serve_archive
+from optimake.logger import LOGGER
+from optimake.serve import OptimakeServer
 
 
 @click.group()
 def cli():
-    """Main CLI entry point."""
+    """
+    Tools for making OPTIMADE APIs for a raw data archives annotated with an
+    `optimade.yaml` file.
+    """
     pass
 
 
 @cli.command()
-@click.option("--jsonl_path", default=None, help="The path to write the JSONL file to.")
-@click.argument("archive_path")
-def convert(
-    jsonl_path,
-    archive_path,
-):
+@click.option(
+    "--jsonl_path",
+    type=click.Path(),
+    help="The path to write the JSONL file to.",
+)
+@click.argument(
+    "path",
+    type=click.Path(),
+)
+def convert(jsonl_path, path):
     """
-    Use an `optimade.yaml` config to describe archived data and create a OPTIMADE JSONL file for ingestion as an OPTIMADE API.
+    Convert a raw data archive into OPTIMADE JSONL.
+
+    PATH needs to contain the full raw data archive, with the `optimade.yaml` config
+    file at the top level. The data is converted into the OPTIMADE JSON Lines format.
     """
 
     if jsonl_path:
@@ -28,19 +39,44 @@ def convert(
         if jsonl_path.exists():
             raise FileExistsError(f"File already exists at {jsonl_path}.")
 
-    convert_archive(Path(archive_path), jsonl_path=jsonl_path)
+    convert_archive(Path(path), jsonl_path=jsonl_path)
 
 
 @cli.command()
-@click.argument("path")
-def serve(
-    path,
-):
+@click.option(
+    "--port",
+    type=int,
+    default=5000,
+    help="The port to serve the API on.",
+)
+@click.argument(
+    "path",
+    type=click.Path(),
+)
+def serve(port, path):
     """
-    TEST
+    Serve a raw data archive with an OPTIMADE API.
+
+    PATH needs to contain the full raw data archive, with the `optimade.yaml` config
+    file at the top level. If needed, the data is first converted into an OPTIMADE JSONL
+    file. However, if the JSONL file already exists, the API is started from it.
+
+    Note that this command starts the API using a simple backend, which is not recommended
+    for a production environment.
     """
 
-    serve_archive(path)
+    jsonl_file = "optimade.jsonl"
+    path = Path(path)
+
+    if not (path / jsonl_file).exists():
+        LOGGER.info(f"{jsonl_file} doesn't exist. Converting archive.")
+        convert_archive(path)
+    else:
+        LOGGER.info(f"{jsonl_file} already exists!")
+
+    LOGGER.info("Starting the API")
+    optimake_server = OptimakeServer(path, port)
+    optimake_server.start_api()
 
 
 if __name__ == "__main__":
